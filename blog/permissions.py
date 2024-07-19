fc54a7e3ee0d0e2a3ea0149d395c2f0093e5843d
Amirhossein <bohloli.amirh@gmail.com>
2024-07-19 14:30:55 +0330

    blogApp = permissions.py -> IsAdminPermission Created

---
+++ blog/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+class IsAdminPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_superuser and request.is_staff
